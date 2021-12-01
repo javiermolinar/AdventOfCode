af9aba1d1d421d1d981022da1e0ad40040aba967
javiermolinar <javiermolinar@live.com>
2021-12-01 17:31:29 +0100

    Even more concise version

--- 2021/python/day1.py
+++ 2021/python/day1.py
@@ -10,10 +10,10 @@ def day1_1():
 def day1_2():
     "Count the number of time it increases as sliding window"
     data = get_data(1, int)
-    count = 0
-    for i, _ in enumerate(data):
-        if i == len(data) - 3:
-            break
-        if sum(data[i : i + 3]) < sum(data[i + 1 : i + 4]):
-            count += 1
-    return count
+    return len(
+        [
+            depth
+            for i, depth in enumerate(data)
+            if i < len(data) - 3 and sum(data[i : i + 3]) < sum(data[i + 1 : i + 4])
+        ]
+    )
