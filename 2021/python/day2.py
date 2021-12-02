317c9b4d23c421e14d0ece8115fbce3a41c2ba55
javiermolinar <javiermolinar@live.com>
2021-12-02 09:51:27 +0100

    Clean up data

--- 2021/python/day2.py
+++ 2021/python/day2.py
@@ -1,27 +1,31 @@
 from utils import get_data
 
+
 def day2_1():
     "Return the multiplication of depth and horizontal lengh"
-    data = get_data(2, str)
-    horizontal = sum([int(st.split(" ")[1]) for st in data if "forward" in st])
-    down = sum([int(st.split(" ")[1]) for st in data if "down" in st])
-    up = sum([int(st.split(" ")[1]) for st in data if "up" in st])
+    data = [(st.split(" ")[0], int(st.split(" ")[1])) for st in get_data(2, str)]
+    horizontal = sum([st[1] for st in data if "forward" in st[0]])
+    down = sum([st[1] for st in data if "down" in st[0]])
+    up = sum([st[1] for st in data if "up" in st[0]])
     return horizontal * (down - up)
 
 
 def day2_2():
-    "Return the multiplication of depth and horizontal lengh"
-    data = get_data(2, str)
+    "Return the multiplication of depth and horizontal lengh with aim"
+    data = [(st.split(" ")[0], int(st.split(" ")[1])) for st in get_data(2, str)]
     aim = 0
     horizontal = 0
     depth = 0
-    for measurement in data:
-        match measurement.split(" "):
-            case ["forward", unit]:
-                horizontal += int(unit)
-                depth += aim * int(unit)
-            case ["down", unit]:
-                aim += int(unit)
-            case ["up", unit]:
-                aim -= int(unit)
+    for measurement, unit in data:
+        match measurement:
+            case "forward":
+                horizontal += unit
+                depth += aim * unit
+            case "down":
+                aim += unit
+            case "up":
+                aim -= unit
     return horizontal * depth
+
+
+print(day2_1())
