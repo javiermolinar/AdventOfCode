973db139c02cac3e02b30733fdf738d650aa9a64
javiermolinar <javiermolinar@live.com>
2021-12-03 13:41:15 +0100

    Better approach

--- 2021/python/day3.py
+++ 2021/python/day3.py
@@ -8,9 +8,9 @@ def to_int(bin_num: List[int]) -> int:
 
 def day3_1() -> int:
     data = get_data(3, str)
-    sum_results = [sum(int(num[i]) for num in data) for i, _ in enumerate(data[0])]
+    sum_results = [sum(int(num[i]) for num in data) for i in range(len(data[0]))]
     gamma = [(1 if x > len(data) / 2 else 0) for x in sum_results]
-    epsilon = [(1 if x == 0 else 0) for x in gamma]
+    epsilon = [x ^ 1 for x in gamma]
     return to_int(gamma) * to_int(epsilon)
 
 
@@ -29,3 +29,6 @@ def day3_2() -> int:
     oxigen_rating = get_rating(data, 0, lambda value, t: 1 if value >= t else 0)
     co2_rating = get_rating(data, 0, lambda value, t: 1 if value < t else 0)
     return oxigen_rating * co2_rating
+
+
+print(day3_1())
