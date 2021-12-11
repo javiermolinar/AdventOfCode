d558d1a66d233ba513cf02f0b94557661783820b
javiermolinar <javiermolinar@live.com>
2021-12-11 23:53:33 +0100

    day 11 fixed

--- 2021/python/day11.py
+++ 2021/python/day11.py
@@ -1,52 +1,48 @@
 from utils import get_data
 
-energy = [[int(y) for y in x] for x in get_data(11, str)]
-
-flashes = 0
-
 
 def increment(i, j, data):
     if i >= 0 and i < len(data) and j >= 0 and j < len(data[0]) and data[i][j] != 0:
         data[i][j] += 1
 
 
-for step in range(500):
+def get_flashes(steps, data):
+    flashes = 0
+    for step in range(steps):
+        for i in range(0, len(data[0])):
+            for j in range(0, len(data)):
+                data[i][j] += 1
 
-    for i in range(0, len(energy[0])):
-        for j in range(0, len(energy)):
-            energy[i][j] += 1
+        more_flashes = True
+        while more_flashes:
+            more_flashes = False
+            for i in range(0, len(data)):
+                for j in range(0, len(data[0])):
+                    if data[i][j] == 0:
+                        continue
+                    if data[i][j] > 9:
+                        data[i][j] = 0
+                        more_flashes = True
+                        flashes += 1
+                        increment(i - 1, j, data)
+                        increment(i - 1, j - 1, data)
+                        increment(i - 1, j + 1, data)
+                        increment(i + 1, j, data)
+                        increment(i + 1, j - 1, data)
+                        increment(i + 1, j + 1, data)
+                        increment(i, j - 1, data)
+                        increment(i, j + 1, data)
+        if sum([flashes for sublist in data for flashes in sublist]) == 0:
+            return (flashes, step + 1)
+    return (flashes, step + 1)
 
-    more_flashes = True
-    while more_flashes:
-        more_flashes = False
-        for i in range(0, len(energy)):
-            for j in range(0, len(energy[0])):
-                if energy[i][j] == 0:
-                    continue
-                if energy[i][j] > 9:
-                    energy[i][j] = 0
-                    more_flashes = True
-                    flashes += 1
-                    if i - 1 >= 0:
-                        if energy[i - 1][j] != 0:
-                            energy[i - 1][j] += 1
-                        if j - 1 >= 0 and energy[i - 1][j - 1] != 0:
-                            energy[i - 1][j - 1] += 1
-                        if j + 1 < len(energy) >= 0 and energy[i - 1][j + 1] != 0:
-                            energy[i - 1][j + 1] += 1
-                    if i + 1 < len(energy[0]):
-                        if energy[i + 1][j] != 0:
-                            energy[i + 1][j] += 1
-                        if j - 1 >= 0 and energy[i + 1][j - 1] != 0:
-                            energy[i + 1][j - 1] += 1
-                        if j + 1 < len(energy) >= 0 and energy[i + 1][j + 1] != 0:
-                            energy[i + 1][j + 1] += 1
-                    if j - 1 >= 0 and energy[i][j - 1] != 0:
-                        energy[i][j - 1] += 1
-                    if j + 1 < len(energy) and energy[i][j + 1] != 0:
-                        energy[i][j + 1] += 1
-    if sum([flashes for sublist in energy for flashes in sublist]) == 0:
-        print(step)
-        break
 
-print(flashes)
+energy_data = [[int(y) for y in x] for x in get_data(11, str)]
+
+
+def day11_1():
+    print(get_flashes(100, energy_data)[0])
+
+
+def day11_2():
+    print(get_flashes(300, energy_data)[1])
