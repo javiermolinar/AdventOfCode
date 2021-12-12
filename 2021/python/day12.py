b474c870f6e69681de020df1a330492fcd745d2c
javiermolinar <javiermolinar@live.com>
2021-12-13 00:13:01 +0100

    day 12

--- 2021/python/day12.py
+++ 2021/python/day12.py
@@ -3,46 +3,37 @@ from collections import namedtuple
 from collections import deque
 
 Node = namedtuple("Node", ["cave", "childs"])
+data = [(x.split("-")) for x in get_data(12)]
 
 
-def get_next_caves(cave_name, visited, map):
+def get_next_caves(cave_name, visited, map, allow_small_caves):
     possible_caves = [x[1] for x in map if x[0] == cave_name]
     possible_caves = [*possible_caves, *[x[0] for x in map if x[1] == cave_name]]
-    valid_caves = [x for x in possible_caves if x not in [y for y in visited if y.islower()]]
+    counts = [visited.count(x) for x in visited if x.islower()]
+    return (
+        [x for x in possible_caves if x not in [y for y in visited if y.islower()]]
+        if not allow_small_caves or 2 in counts
+        else [x for x in possible_caves if x != "start"]
+    )
 
-    return valid_caves
 
-
-def build_paths(node, map, visited):
+def build_paths(node, map, visited, allow_small_caves=False):
     if node.cave == "end":
         visited.popleft()
         return 1
     paths = 0
-    for cave_name in get_next_caves(node.cave, visited, map):
+    for cave_name in get_next_caves(node.cave, visited, map, allow_small_caves):
         cave = Node(cave_name, [])
         node.childs.append(cave)
         visited.appendleft(cave_name)
-        paths += build_paths(cave, map, visited)
+        paths += build_paths(cave, map, visited, allow_small_caves)
     visited.popleft()
     return paths
 
 
 def day12_1():
-    data = [(x.split("-")) for x in get_data(12)]
-    print(data)
-    root = Node("start", [])
-    visited = deque(["start"])
-    paths = build_paths(root, data, visited)
-    print(paths)
+    print(build_paths(Node("start", []), data, deque(["start"])))
 
 
 def day12_2():
-    data = [(x.split("-")) for x in get_data(12)]
-    print(data)
-    root = Node("start", [])
-    visited = deque(["start"])
-    paths = build_paths(root, data, visited)
-    print(paths)
-
-
-day12_1()
+    print(build_paths(Node("start", []), data, deque(["start"]), True))
